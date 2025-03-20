a95199dbe46529da71b60abea5ca1900f81ba1c5
Jay.D <djccnt15@gmail.com>
2025-03-20 22:28:14 +0900

    initial commit, initiate project

---
+++ src/test/java/com/djccnt15/spring_scheduling/SpringSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djccnt15.spring_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
